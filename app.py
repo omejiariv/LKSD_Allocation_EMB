7216fec3b49dd9f416bbb7820511aec9115556fd
omejiariv <145706652+omejiariv@users.noreply.github.com>
2026-05-09 12:47:40 -0500

    Fix key name for Womens Allocation Grade

--- app.py
+++ app.py
@@ -245,7 +245,7 @@ if st.session_state.datos_cargados:
             metrics_dict = {}
 
         # Mapeos base
-        store_grades = pd.Series(df_stores['Womens Allocation Grade'].values, index=df_stores['Store']).to_dict()
+        store_grades = pd.Series(df_stores['Womens_Allocation_Grade'].values, index=df_stores['Store']).to_dict()
         store_climates = pd.Series(df_stores['Climate'].astype(str).str.lower().str.strip().values, index=df_stores['Store']).to_dict()
         tiendas_destino = df_stores['Store'].dropna().tolist()
 
